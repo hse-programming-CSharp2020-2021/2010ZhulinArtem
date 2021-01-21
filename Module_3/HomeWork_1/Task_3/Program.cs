941cd0ac8f81023fecb84279ae846aa69987e44e
azhulin <azhulin@edu.hse.ru>
2021-01-21 16:03:31 +0600

    New Homework

---
+++ Module_3/HomeWork_1/Task_3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_3
+{
+    internal class TemperatureConverterImp
+    {
+        public double ConvertCToF(double temperature) => 9 * temperature / 5 + 32;
+        public double ConvertFToC(double temperature) => 5 * (temperature - 32) / 9;
+    }
+
+    internal class StaticTempConverters
+    {
+        public static double ConvertCToK(double temperature) => temperature + 273.15;
+        public static double ConvertKToC(double temperature) => temperature - 273.15;
+
+        public static double ConvertCToRan(double temperature) => (temperature + 273.15) * 9 / 5;
+        public static double ConvertRanToC(double temperature) => (temperature - 491.67) * 5 / 9;
+
+        public static double ConvertCToRea(double temperature) => temperature * 4 / 5;
+        public static double ConvertReaToC(double temperature) => temperature * 5 / 4;
+    }
+
+    internal class Program
+    {
+        private delegate double DelegateConvertTemperature(double temperature);
+
+        private static readonly Random Random = new Random();
+
+        private static void Main()
+        {
+            TestDelegateFirst();
+            Console.WriteLine();
+            TestDelegateSecond();
+        }
+
+        private static void TestDelegateFirst()
+        {
+            DelegateConvertTemperature delegateConvert = new TemperatureConverterImp().ConvertCToF;
+            delegateConvert += new TemperatureConverterImp().ConvertFToC;
+
+            var temperature = Random.NextDouble() * 100;
+            Console.WriteLine($"Temperature: {temperature:F2}");
+
+            foreach (DelegateConvertTemperature converter in delegateConvert.GetInvocationList())
+            {
+                Console.WriteLine(
+                    $"Result: {converter?.Invoke(temperature):F2}; " +
+                    $"Method: {converter?.Method}; Target: {converter?.Target}"
+                    );
+            }
+        }
+
+        private static void TestDelegateSecond()
+        {
+            var delegateConverters = new List<DelegateConvertTemperature> { new TemperatureConverterImp().ConvertCToF, 
+                new TemperatureConverterImp().ConvertFToC };  
+
+            delegateConverters.AddRange(new List<DelegateConvertTemperature>
+            {
+                StaticTempConverters.ConvertCToK, StaticTempConverters.ConvertKToC,
+                StaticTempConverters.ConvertCToRan, StaticTempConverters.ConvertRanToC,
+                StaticTempConverters.ConvertCToRea, StaticTempConverters.ConvertReaToC
+            });
+
+            double temperature;
+
+            do
+            {
+                Console.Write("Enter the correct number: ");
+            } while (!double.TryParse(Console.ReadLine(), out temperature) || temperature < -273 || temperature > 1000);
+
+            foreach (var converter in delegateConverters)
+            {
+                Console.WriteLine($"Result: {converter?.Invoke(temperature):F2}; Method: {converter?.Method}");
+            }
+        }
+    }
+}
---
+++ Module_3/HomeWork_1/Task_4/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+
+namespace Task_4
+{
+    internal class Robot
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public char[,] Map;
+        public Point CurrentPoint => new Point {X = X, Y = Y};
+
+        internal class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public override string ToString() => $"X = {X}; Y = {Y}";
+        }
+
+        public Robot(uint length, uint wide)
+        {
+            Map = new char[length, wide];
+        }
+
+        public void Right()
+        {
+            if (CurrentPoint.X + 1 >= Map.GetLength(0))
+            {
+                throw new ArgumentException("Out of bounds");
+            }
+
+            Map[CurrentPoint.Y, CurrentPoint.X] = '+';
+            X++;
+        }
+
+        public void Left()
+        {
+            if (CurrentPoint.X - 1 < 0)
+            {
+                throw new ArgumentException("Out of bounds");
+            }
+
+            Map[CurrentPoint.Y, CurrentPoint.X] = '+';
+            X--;
+        }
+
+        public void Forward()
+        {
+            if (CurrentPoint.Y + 1 >= Map.GetLength(1))
+            {
+                throw new ArgumentException("Out of bounds");
+            }
+
+            Map[CurrentPoint.Y, CurrentPoint.X] = '+';
+            Y++;
+        }
+
+        public void Backward()
+        {
+            if (CurrentPoint.Y - 1 < 0)
+            {
+                throw new ArgumentException("Out of bounds");
+            }
+
+            Map[CurrentPoint.Y, CurrentPoint.X] = '+';
+            Y--;
+        }
+
+        public string Position() => $"The Robot Position: {CurrentPoint}";
+
+        public void DrawMap()
+        {
+            for (var i = 0; i < Map.GetLength(0); i++, Console.WriteLine())
+            {
+                for (var j = 0; j < Map.GetLength(1); j++)
+                {
+                    if (j == CurrentPoint.X && i == CurrentPoint.Y)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write('*');
+                        continue;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.Write(Map[i, j] != (char) 0 ? Map[i, j] : ' ');
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+        private delegate void Steps();
+
+        private static void Main()
+        {
+            var robot = CreateRobot();
+            do
+            {
+                try
+                {
+                    Console.Clear();
+                    robot.DrawMap();
+                    Console.WriteLine(robot.Position());
+
+
+                    var steps = GetSteps(robot);
+                    steps?.Invoke();
+
+                    robot.DrawMap();
+                    Console.WriteLine(robot.Position());
+                }
+                catch (ArgumentException exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+
+                Console.WriteLine("Enter ESC - to exit, or another - to repeat...");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+
+        private static Robot CreateRobot()
+        {
+            uint lengthMap, wideMap;
+            do
+            {
+                Console.Write("Enter the length of map: ");
+            } while (!uint.TryParse(Console.ReadLine(), out lengthMap));
+
+            do
+            {
+                Console.Write("Enter the wide of map: ");
+            } while (!uint.TryParse(Console.ReadLine(), out wideMap));
+
+            var robot = new Robot(lengthMap, wideMap);
+            return robot;
+        }
+
+        private static Steps GetSteps(Robot robot)
+        {
+            Steps stepDel = null;
+
+            Console.Write("Enter steps (EX: \"LR\" -> Left - Right): ");
+            var steps = Console.ReadLine();
+
+            if (steps == null) return null;
+
+            foreach (var step in steps.ToLower())
+            {
+                stepDel += step switch
+                {
+                    'l' => robot.Left,
+                    'r' => robot.Right,
+                    'f' => robot.Forward,
+                    'b' => robot.Backward,
+                    _ => throw new ArgumentException("Incorrect input")
+                };
+            }
+
+            return stepDel;
+        }
+    }
+}
